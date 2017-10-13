c50a758e62355f2ef1ae75d09d8929f5e8b3fcf9
Francis Sunday <iamfrankcute@gmail.com>
2017-10-13 05:46:03 +0100

    updated root endpoints fallback messages:

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ const DeveloperController = require('../controllers/DeveloperController');
 const routes = (app) => {
   app.get('/', (req, res, next) => {
     res.send({
-      message: "Welcome to the PHDevConnect Developers api, use enpoint /api/vi/developers"
+      message: "Welcome to the PHDevConnect Developers API, use endpoint /api/v1/developers"
     });
   });
 
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 require("babel-polyfill");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express"),
-  http = require('http'),
-  socketio = require('socket.io'),
+  http = require("http"),
+  socketio = require("socket.io"),
   router = express.Router(),
-  helmet = require('helmet'),
-  logger = require('morgan'),
-  session = require('express-session'),
-  cookieParser = require('cookie-parser'),
-  bcrypt = require('bcrypt'),
-  crypto = require('crypto'),
-  path = require('path'),
-  shortid = require('shortid'),
-  fs = require('fs'),
-  fileParser = require('connect-multiparty')(),
-  validator = require('validator'),
+  helmet = require("helmet"),
+  logger = require("morgan"),
+  session = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  bcrypt = require("bcrypt"),
+  crypto = require("crypto"),
+  path = require("path"),
+  shortid = require("shortid"),
+  fs = require("fs"),
+  fileParser = require("connect-multiparty")(),
+  validator = require("validator"),
   mime = require("mime"),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  _ = require('lodash'),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  _ = require("lodash"),
   app = express();
 
 const environment = process.env.NODE_ENV || "development";
@@ -29,29 +29,37 @@ mongoose.Promise = global.Promise;
 const online_DB_uri = `mongodb://heroku_8s9nbdfn:4vjc4tbv8oho7jg10tqnv1qd3p@ds113795.mlab.com:13795/heroku_8s9nbdfn`,
   local_DB_uri = `mongodb://localhost:27017/phdevsdir`;
 
-mongoose.connect( environment === "production" ? online_DB_uri : local_DB_uri, {
-  useMongoClient: true
-}, (err, db) => {
-  if (err) {
-    console.log("Couldn't connect to database");
-  } else {
-    console.log(`Connected To ${environment} Database`);
+mongoose.connect(
+  environment === "production" ? online_DB_uri : online_DB_uri,
+  {
+    useMongoClient: true
+  },
+  (err, db) => {
+    if (err) {
+      console.log("Couldn't connect to database");
+    } else {
+      console.log(`Connected To ${environment} Database`);
+    }
   }
-});
+);
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(helmet());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 process.env.PWD = process.cwd();
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static(path.join(process.env.PWD, "public")));
 
 const api = express.Router();
-require('./app/routes/index.js')(api);
-app.use('/api/v1', api);
+require("./app/routes/index.js")(api);
+app.use("/api/v1", api);
+app.get("/", (req, res) => {
+  res.send({
+    message: "Valid endpoint is /api/v1/"
+  });
+});
 
 /**
  * Serve API
@@ -62,8 +70,8 @@ app.use('/api/v1', api);
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -75,9 +83,11 @@ const io = socketio(server);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port, function() {console.log('listening on port ' + port)});
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port, function() {
+  console.log("listening on port " + port);
+});
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -104,22 +114,20 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -130,11 +138,9 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-const debug = require('debug');
+const debug = require("debug");
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
