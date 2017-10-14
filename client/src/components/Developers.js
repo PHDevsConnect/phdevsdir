93609cad8f2f18b2ebb5fd81d76be3bfa5af9266
tonye-d <itztonye@gmail.com>
2017-10-14 20:17:46 +0100

    changed to functional component

--- client/src/components/Developers.js
+++ client/src/components/Developers.js
@@ -2,40 +2,35 @@ import React from 'react';
 import devList from './DevelopersList';
 
 
-class Developers extends React.Component {
- 
-  render (){
-    
-    const listItems = devList.map((d) => 
-      <div key={d.id} className="developer">
-        <div className="profile-pic">
-          <img src={d.image} />
-        </div>
-        <div className="profile-data">
-          <span className="developer-name">
-            {d.name}
-            <span title="Verified Skills" className="v-badge">Verified</span></span>
-          <span>
-            {
-              d.stack.map((e, i) =>
-                <span key={i} className="tech">{e}</span>
-              )               
-            }
-          </span>
-          <span><a target="_blank" href={d.giturl}>View on Github</a></span>
-        </div>
-      </div>
-    );
-    
-    
-    return (
-      <div className="developers">
-      {listItems}
-      </div>
-    ); 
-    
-  }
- 
-}
+const Developers = () => {
+  
+     const listItems = devList.map((d) => 
+       <div key={d.id} className="developer">
+         <div className="profile-pic">
+           <img src={d.image} />
+         </div>
+         <div className="profile-data">
+           <span className="developer-name">
+             {d.name}
+             <span title="Verified Skills" className="v-badge">Verified</span></span>
+           <span>
+             {
+               d.stack.map((e, i) =>
+                 <span key={i} className="tech">{e}</span>
+               )               
+             }
+           </span>
+           <span><a target="_blank" href={d.giturl}>View on Github</a></span>
+         </div>
+       </div>
+     );
+     
+     
+     return (
+       <div className="developers">
+       {listItems}
+       </div>
+     ); 
+ }
 
 export default Developers;
