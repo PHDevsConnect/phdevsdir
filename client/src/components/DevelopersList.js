96092a596eacd0431c04eee8be5bfcede12cac92
tonye-d <itztonye@gmail.com>
2017-10-14 19:52:24 +0100

    cleaned up the developers component

---
+++ client/src/components/DevelopersList.js
@@ -0,0 +1,32 @@
+const devList = [
+        {
+          "id": 1,
+          "image": "https://avatars3.githubusercontent.com/u/9336187?v=4&s=460",
+          "name": "Francis Sunday",
+          "giturl": "https://github.com/codehakase",
+          "stack": ["Go Lang", "PHP", "React"]
+        },
+        {
+          "id": 2,
+          "image": "https://avatars2.githubusercontent.com/u/24508232?v=4&s=460",
+          "name": "Joseph Uchenna",
+          "giturl": "https://github.com/afrikhero",
+          "stack": ["JavaScript", "PHP", "MySQL"]
+        },
+        {
+          "id": 3,
+          "image": "https://avatars1.githubusercontent.com/u/25697914?v=4&s=460",
+          "name": "Ndifreke Friday",
+          "giturl": "https://github.com/NddyAndy",
+          "stack": ["JavaScript"]
+        },
+        {
+          "id": 4,
+          "image": "https://avatars3.githubusercontent.com/u/15184445?v=4&s=460",
+          "name": "Michael Ozoemena",
+          "giturl": "https://github.com/THEozmic",
+          "stack": ["Python", "PHP", "React"]
+        }
+      ]
+
+export default devList;
