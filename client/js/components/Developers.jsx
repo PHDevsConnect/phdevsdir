41318b593f2145979146fd19e17f157edd1709a6
THEozmic <michael.ozoemena@andela.com>
2017-10-15 04:45:07 +0100

    Change font and Add icon

--- client/js/components/Developers.jsx
+++ client/js/components/Developers.jsx
@@ -3,34 +3,38 @@ import devList from './DevelopersList';
 
 
 const Developers = () => {
+  const listItems = devList.map((d) => 
+    <div key={d.id} className="developer">
+      <div className="profile-pic">
+        <img src={d.image} />
+      </div>
+      <span class="badge-container">
+        <span title="Verified" className="v-badge">
+          <i className="icon-energy"></i>
+        </span>
+      </span>
+      <div className="profile-data">
+      <span className="developer-name">
+        {d.name}
+      </span>
+      <span>
+        {
+          d.stack.map((e, i) =>
+            <span key={i} className="tech">{e}</span>
+          )
+        }
+      </span>
+      <span><a target="_blank" href={d.giturl}>View on Github</a></span>
+      </div>
+    </div>
+  );
   
-     const listItems = devList.map((d) => 
-       <div key={d.id} className="developer">
-         <div className="profile-pic">
-           <img src={d.image} />
-         </div>
-         <div className="profile-data">
-           <span className="developer-name">
-             {d.name}
-             <span title="Verified Skills" className="v-badge">Verified</span></span>
-           <span>
-             {
-               d.stack.map((e, i) =>
-                 <span key={i} className="tech">{e}</span>
-               )
-             }
-           </span>
-           <span><a target="_blank" href={d.giturl}>View on Github</a></span>
-         </div>
-       </div>
-     );
-     
-     
-     return (
-       <div className="developers">
-       {listItems}
-       </div>
-     ); 
+  
+  return (
+    <div className="developers">
+      {listItems}
+    </div>
+  );
  }
 
 export default Developers;
