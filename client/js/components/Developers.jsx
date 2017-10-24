f549b6912a86e7ec2c89624c493ac5b8c3cc6e81
coderbattles <michael.ozoemena@andela.com>
2017-10-24 07:37:23 +0100

    Fetch image from github

--- client/js/components/Developers.jsx
+++ client/js/components/Developers.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 import devList from './DevelopersList';
 
 
-const Developers = () => {
-  const listItems = devList.map((d) => 
-    <div key={d.id} className="developer">
+class Developers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      devList: []
+    }
+  }
+
+  componentWillMount() {
+    axios.get('/api/v1/developers').then(
+      (result) => {
+        this.setState({
+          devList: result.data.data.users
+        })
+
+        let newDevList = [];
+        console.log(this.state.devList, '==>>>');
+        this.state.devList.map((dev) => {
+          axios.get(`https://api.github.com/users/${dev.github_url.split('/')[3]}`)
+          .then((result) => {
+            const user = result.data;
+            dev.image = user.avatar_url;
+            newDevList.push(dev);
+            this.setState({ devList: newDevList });
+          });
+        });
+      }
+    )
+    
+  }
+
+  render() {
+    const listItems = this.state.devList.map((d) => 
+    <div key={d._id} className="developer">
       <div className="profile-pic">
         <img src={d.image} />
       </div>
@@ -15,7 +47,7 @@ const Developers = () => {
       </span>
       <div className="profile-data">
       <span className="developer-name">
-        {d.name}
+        {d.first_name + " " + d.last_name}
       </span>
       <span>
         {
@@ -24,17 +56,16 @@ const Developers = () => {
           )
         }
       </span>
-      <span><a target="_blank" href={d.giturl}>View on Github</a></span>
+      <span><a target="_blank" href={d.github_url}>View on Github</a></span>
       </div>
     </div>
   );
-  
-  
   return (
     <div className="developers">
       {listItems}
     </div>
   );
+  }
  }
 
 export default Developers;
